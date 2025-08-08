0da7e6886721ea2c8b1cb20267bc0c30d6af3b04
acewol <shura.evstigneev@list.ru>
2025-08-09 00:06:47 +0300

    Commit

--- config.py
+++ config.py
@@ -1,2 +1,22 @@
-MOODLE_URL = ''
-MOODLE_TOKEN = ''
+def get_all_course():
+    # Получаем список всех курсов через core_course_get_courses
+    pass
+
+
+def get_all_students():
+    # Получаем список всех пользователей студентов через core_user_get_users
+    pass
+
+
+def get_active_users(days=1):
+    # Получаем пользователей, которые были активны за последние n дней
+    pass
+
+
+# bot_handlers.py
+async def summary(update,context):
+    # Вызов трех функций выше, сбор статистики
+    # Формирование сообщения
+    pass
+
+
