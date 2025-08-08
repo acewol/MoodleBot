74cf27d9aa0bc215929c237606b4f45316b4bbed
acevol <shura.evstigneev@list.ru>
2025-08-09 00:14:11 +0300

    Commit

--- config.py
+++ config.py
@@ -1,22 +1,4 @@
-def get_all_course():
-    # Получаем список всех курсов через core_course_get_courses
-    pass
-
-
-def get_all_students():
-    # Получаем список всех пользователей студентов через core_user_get_users
-    pass
-
-
-def get_active_users(days=1):
-    # Получаем пользователей, которые были активны за последние n дней
-    pass
-
-
-# bot_handlers.py
-async def summary(update,context):
-    # Вызов трех функций выше, сбор статистики
-    # Формирование сообщения
-    pass
-
-
+MOODLE_URL = 'KEY'
+MOODLE_API = 'KEY'
+TOKEN = 'KEY'
+STUDENT_ROLE_ID = 5 # Обычно роль студента это 5
